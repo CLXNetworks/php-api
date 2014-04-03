2fc5e882d9f8d070d7baad4dae234037ac2016db
Joel Stahre <joelstahre@gmail.com>
2014-04-03 16:06:42 +0200

    Added ClxApi class

---
+++ clxapi/ClxApi.php
@@ -0,0 +1,17 @@
+<?php
+
+class ClxApi {
+
+    /**
+     * The SPI base URL
+     */
+    const API_URL = '';
+
+
+    /**
+     * Deafault Constructor
+     */
+    public function __construct() {
+
+    }
+}
