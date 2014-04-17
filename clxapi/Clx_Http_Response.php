a48f060939a67747b2adfc7710e3c9171471dced
Joel Stahre <joelstahre@gmail.com>
2014-04-17 14:32:33 +0200

    added Clx_Http_Response class

---
+++ clxapi/Clx_Http_Response.php
@@ -0,0 +1,50 @@
+<?php
+
+class Clx_Http_Response {
+
+    /**
+     * @var string
+     */
+    private $data;
+
+    /**
+     * @var int
+     */
+    private $code;
+
+    /**
+     * @var string
+     */
+    private $error;
+
+    /** 
+     * Default Constructor
+     */
+    public function __construct($data, $code, $error) {
+        $this->data = $data;
+        $this->code = $code;
+        $this->error = $error;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatusCode() {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getError() {
+        return $this->error;
+    }
+
+}
