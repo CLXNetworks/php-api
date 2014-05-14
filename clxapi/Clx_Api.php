63e7534da085b58592958fb41f71141a523ba92b
Joel Stahre <joelstahre@gmail.com>
2014-05-14 16:15:01 +0200

    added _setHttpAdapter functionality

--- clxapi/Clx_Api.php
+++ clxapi/Clx_Api.php
@@ -2,7 +2,7 @@
 
 require_once 'Clx_Http_Client.php';
 
-class Clx_Api extends Clx_Http_Client{
+class Clx_Api {
 
 
     /**
@@ -11,16 +11,44 @@ class Clx_Api extends Clx_Http_Client{
     private $baseURI;
 
     /**
-     * Default Constructor
-     * @param array
+     * @var Clx_Http_Client
      */
-    public function __construct( $config )
+    private $httpClient;
+
+    /**
+     * Default Constructor
+     * @param array|null $config
+     * @param null|Clx_Http_Adapter_Interface $httpAdapter
+     * @todo not sure if adapter should be set in construct of Clx_Api
+     */
+    public function __construct( $config = null, $httpAdapter = null )
     {
-        parent::__construct( $config['username'], $config['password'] );
-        parent::setHttpAdapter( $config['httpAdapter'] );
-        $this->setBaseURI ( $config['baseURL'] );
+        $this->httpClient = new Clx_Http_Client();
+        $this->_setHttpAdapter( $httpAdapter );
+
+        if ( $config !== null )
+        {
+            $this->httpClient->setAuth( $config['username'], $config['password'] );
+        }
+
+
     }
 
+    /**
+     * @param null|Clx_Http_Adapter_Interface $httpAdapter
+     * If no adapter is provided, set the httpAdapter to Clx_Http_Adapter_Curl
+     */
+    private function _setHttpAdapter( $httpAdapter )
+    {
+        if ( $httpAdapter !== null )
+        {
+            $this->httpClient->setHttpAdapter( $httpAdapter );
+        }
+        else
+        {
+            $this->httpClient->setHttpAdapter( new Clx_Http_Adapter_Curl() );
+        }
+    }
 
     /**
      * @param string $url
@@ -28,15 +56,13 @@ class Clx_Api extends Clx_Http_Client{
      */
     public function setBaseURI( $url )
     {
-        if( is_string( $url ) )
-        {
-            $this->baseURI = $url;
-        }
-        else
+        if( !is_string( $url ) )
         {
             require_once 'Clx_Exception.php';
             throw new Clx_Exception( 'URL must be of type String!' );
         }
+
+        $this->baseURI = $url;
     }
 
     /**
@@ -53,8 +79,8 @@ class Clx_Api extends Clx_Http_Client{
      */
     public function getOperators()
     {
-        parent::setURI( $this->_getBaseURI() . '/operator' );
-        $operators = parent::request( 'GET' );
+        $this->httpClient->setURI( $this->_getBaseURI() . '/operator' );
+        $operators = $this->httpClient->request( 'GET' );
 
         return $operators;
     }
@@ -70,8 +96,8 @@ class Clx_Api extends Clx_Http_Client{
 
         if( is_numeric( $operator_id ) )
         {
-            parent::setURI( $this->_getBaseURI() . '/operator/' . $operator_id) ;
-            $operator = parent::request( 'GET' );
+            $this->httpClient->setURI( $this->_getBaseURI() . '/operator/' . $operator_id) ;
+            $operator = $this->httpClient->request( 'GET' );
 
             return $operator;
         } 
