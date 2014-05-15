f48a8340668e511ff5f5fe6beb8915c77a2e7afc
Joel Stahre <joelstahre@gmail.com>
2014-05-15 23:28:47 +0200

    updated tests

--- tests/Clx_Http_ResponseTest.php
+++ tests/Clx_Http_ResponseTest.php
@@ -8,21 +8,38 @@ class Clx_Http_ResponseTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
 
-        $body = 'body';
-        $headers = 'headers';
+        $body = '{"id":1,"name":"John","network":"Doe","uniqueName":"JohnDOE","isoCountryCode":"12","operationalState":"active","operationalStatDate":"-0001-11-30 00:00:00","numberOfSubscribers":0}';
+        $rawHeaders = "HTTP/1.1 200 OK\r
+                        Server: Apache\r
+                        Content-Type: application/json\r";
+
         $statusCode = 200;
         $error = 'error';
-        $this->Clx_Http_Response = new Clx_Http_Response( $body, $headers, $statusCode, $error );
+        $this->Clx_Http_Response = new Clx_Http_Response( $body, $rawHeaders, $statusCode, $error );
     }
 
 
     public function testValidConstruct()
     {
+        $body = '{"id":1,"name":"John","network":"Doe","uniqueName":"JohnDOE","isoCountryCode":"12","operationalState":"active","operationalStatDate":"-0001-11-30 00:00:00","numberOfSubscribers":0}';
+        $expectedHeadersArray = array(
+            'Server' => 'Apache',
+            'Content-Type' => 'application/json'
+        );
+
+        $expectedRawHeadersString = "HTTP/1.1 200 OK\r
+                        Server: Apache\r
+                        Content-Type: application/json\r";
+        $statusCode = 200;
+        $error = 'error';
+
+
         $this->assertInstanceOf('Clx_Http_Response', $this->Clx_Http_Response);
-        $this->assertEquals( 'body', $this->Clx_Http_Response->getBody() );
-        $this->assertEquals( 'headers', $this->Clx_Http_Response->getHeaders() );
-        $this->assertEquals( 200, $this->Clx_Http_Response->getStatusCode() );
-        $this->assertEquals( 'error', $this->Clx_Http_Response->getError() );
+        $this->assertEquals( $body, $this->Clx_Http_Response->getBody() );
+        $this->assertEquals( $expectedRawHeadersString, $this->Clx_Http_Response->getRawHeaders() );
+        $this->assertEquals( $expectedHeadersArray, $this->Clx_Http_Response->getHeaders() );
+        $this->assertEquals( $statusCode, $this->Clx_Http_Response->getStatusCode() );
+        $this->assertEquals( $error, $this->Clx_Http_Response->getError() );
     }
 
     /**
@@ -30,7 +47,7 @@ class Clx_Http_ResponseTest extends PHPUnit_Framework_TestCase {
      */
     public function testFaultyValueBodyConstructThrowsIfNotString()
     {
-        new Clx_Http_Response( 1, 'headers', 200, 'error' );
+        new Clx_Http_Response( 1, 'rawHeaders', 200, 'error' );
     }
 
     /**
@@ -46,7 +63,7 @@ class Clx_Http_ResponseTest extends PHPUnit_Framework_TestCase {
      */
     public function testFaultyValueStatusCodeConstructThrowsIfNotInteger()
     {
-        new Clx_Http_Response( 'body', 'headers', '200', 'error' );
+        new Clx_Http_Response( 'body', 'rawHeaders', '200', 'error' );
     }
 
     /**
@@ -54,7 +71,7 @@ class Clx_Http_ResponseTest extends PHPUnit_Framework_TestCase {
      */
     public function testFaultyValueErrorConstructThrowsIfNotString()
     {
-        new Clx_Http_Response( 'body', 'headers', 200, 1 );
+        new Clx_Http_Response( 'body', 'rawHeaders', 200, 1 );
     }
 
 }
