8b9e46137f6fbf9557b1fa13e5e67adad9be99ae
Joel Stahre <joelstahre@gmail.com>
2014-05-19 22:03:08 +0200

    added some examples

---
+++ examples/get_operator_by_id.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'clxapi/Clx_Api.php';
+
+$config = array(
+    'username' => 'your-username',
+    'password'=> 'your-password'
+);
+
+
+$clx = new Clx_Api( $config );
+$clx->setBaseURI( 'https://example.com/api' );
+
+try {
+    $operator = $clx->getOperatorById( 1 );
+
+    echo ' id: ' . $operator->id;
+    echo ' name: ' . $operator->name;
+    echo ' network: ' . $operator->network;
+    echo ' uniqueName: ' . $operator->uniqueName;
+    echo ' isoCountryCode: ' . $operator->isoCountryCode;
+    echo ' operationalState: ' . $operator->operationalState;
+    echo ' operationalStatDate: ' . $operator->operationalStatDate;
+    echo ' numberOfSubscribers: ' . $operator->numberOfSubscribers . "\n";
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+?>
---
+++ examples/get_operators.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'clxapi/Clx_Api.php';
+
+$config = array(
+    'username' => 'your-username',
+    'password'=> 'your-password'
+);
+
+
+$clx = new Clx_Api( $config );
+$clx->setBaseURI( 'https://example.com/api' );
+
+try
+{
+    $operators = $clx->getOperators();
+
+    foreach ( $operators as $operator )
+    {
+        echo 'id: ' . $operator->id;
+        echo ' name: ' . $operator->name;
+        echo ' network: ' . $operator->network;
+        echo ' uniqueName: ' . $operator->uniqueName;
+        echo ' isoCountryCode: ' . $operator->isoCountryCode;
+        echo ' operationalState: ' . $operator->operationalState;
+        echo ' operationalStatDate: ' . $operator->operationalStatDate;
+        echo ' numberOfSubscribers: ' . $operator->numberOfSubscribers . "\n";
+
+    }
+}
+catch ( Exception $e ) {
+    echo $e->getMessage();
+}
+
+
+
+?>
