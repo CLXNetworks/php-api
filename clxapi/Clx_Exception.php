8f011be21be77308e58ad9b37f8929d7009f0a41
Joel Stahre <joelstahre@gmail.com>
2014-05-20 18:39:24 +0200

    added getResponseObject method

--- clxapi/Clx_Exception.php
+++ clxapi/Clx_Exception.php
@@ -3,5 +3,18 @@
 class Clx_Exception extends Exception
 {
 
+    private $Clx_Http_Response;
+
+    public function __construct( $message = '', Clx_Http_Response $responseObject = NULL, $code = 0, Exception $previous = NULL )
+    {
+        $this->Clx_Http_Response = $responseObject;
+        parent::__construct( $message, $code, $previous );
+    }
+
+    public function getResponseObject()
+    {
+        return $this->Clx_Http_Response;
+    }
+
 }
 ?>
--- tests/Clx_ExceptionTest.php
+++ tests/Clx_ExceptionTest.php
@@ -2,15 +2,32 @@
 
 class Clx_ExceptionTest extends PHPUnit_Framework_TestCase
 {
-    public function setUp() {
 
-    }
-
-    //test construct
-    public function testConstruct()
+    public function testGetMessage()
     {
-
+        try
+        {
+            throw new Clx_Exception( 'test' );
+        }
+        catch( Clx_Exception $e )
+        {
+            $this->assertEquals( 'test', $e->getMessage());
+        }
     }
-}
 
-?>
+    public function testGetResponseObject()
+    {
+        $Clx_Http_Response = new Clx_Http_Response( 'body', 'rawHeaders', 400, '');
+
+        try
+        {
+            throw new Clx_Exception( 'test', $Clx_Http_Response );
+        }
+        catch( Clx_Exception $e )
+        {
+            $this->assertInstanceOf( 'Clx_Http_Response', $e->getResponseObject() );
+        }
+    }
+
+}
+ 
